8fd9466cd9d15736c77773f3ce9d020bf59abe2a
iammayankg@gmail.com <iammayankg@gmail.com>
2017-10-29 13:56:56 -0400

    done

---
+++ fetch_rsp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import xlwt
+
+def write_res(filename, res):
+    wb = xlwt.Workbook()
+    ws = wb.add_sheet('Sheet 1')
+    format_string = u"₹"
+    for idx, prod in enumerate(res['productList']):
+        cell = u"{} {}".format(format_string, prod['rsp'])
+        ws.write(idx, 0, cell)
+    wb.save(filename)
+
+
+if __name__ == "__main__":
+    import requests
+
+    out_file = 'out.xlsx'
+    url = "https://immense-earth-44640.herokuapp.com/api/"
+
+    response = requests.get(url)
+    if response.status_code != 200:
+        print("Response failed with code ", response.status_code)
+    else:
+        res = response.json()
+        write_res(out_file, res)
+    pass
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='iocl',
+      version='0.1',
+      description='',
+      url='https://github.com/iammayankg/iocl.git',
+      author='',
+      author_email='iammayankg@gmail.com',
+      license='MIT',
+      install_requires=["requests", "xlwt"],
+      zip_safe=False)
